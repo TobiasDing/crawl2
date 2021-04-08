6cb8d4c697fea3ee44396b7b80c5e964e82ee702
TobiasDing <tobias.ding@hotmail.com>
2021-04-08 22:03:50 +0800

    Initial commit

---
+++ src/main/java/om/tobias/Pages/Pages.java
@@ -0,0 +1,15 @@
+package om.tobias.Pages;
+
+
+import org.jsoup.nodes.Document;
+public class Pages {
+
+    private byte[] content;
+    private String html;
+    private Document doc;
+    private String charset;
+    private String url;
+    private String contentType;
+
+    
+}
